ef466ecca3302666fa66dbd8298ea5e293e87c90
allanbezerra1 <allanbezerra95@gmail.com>
2021-05-05 22:59:20 -0300

    up

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\participanteController;
+use App\Http\Controllers\empresaController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('participante', [ParticipanteController::class, 'index']);
+Route::get('empresa', [EmpresaController::class, 'index']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('participante/{id}', [ParticipanteController::class, 'show']);
+Route::get('empresa/{id}', [EmpresaController::class, 'show']);
+
+Route::post('participante', [ParticipanteController::class, 'store']);
+Route::post('empresa', [EmpresaController::class, 'store']);
+
+Route::put('participante/{id}', [ParticipanteController::class, 'update']);
+Route::put('empresa/{id}', [EmpresaController::class, 'update']);
+
+Route::delete('participante/{id}', [ParticipanteController::class, 'destroy']);
+Route::delete('empresa/{id}', [EmpresaController::class, 'destroy']);
